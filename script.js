2fcee7784def9539aae8667506e360ea22faf9a4
PasqualeDintrono <dintronopasquale94@gmail.com>
2023-05-25 10:49:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Navbar function for mobile and desktop visualization
+$(document).ready(function(){
+    $(".mobile-nav i").click(function(){
+        $(".site-nav-menu").toggleClass("mobile-menu");
+    });
+ });
+
+
+
+
